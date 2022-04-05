61e81fa3e5302fb7933ca10fe466bbdbc5493c66
Niwradel <39563212+Niwradel@users.noreply.github.com>
2022-04-05 16:38:56 +0200

    Update Filter_loci_with_NR.py

--- Filter_loci_with_NR.py
+++ Filter_loci_with_NR.py
@@ -1,8 +1,8 @@
-
 import pandas as pd 
 import argparse
 import os
 import io 
+from Bio import SeqIO
 
 # Print out a message when the program is initiated.
 print('-----------------------------------------------------------------------------------\n')
@@ -10,77 +10,68 @@ print('                        Filter loci with NR results                      
 print('-----------------------------------------------------------------------------------\n')
 
 #----------------------------------- PARSE SOME ARGUMENTS ----------------------------------------
-parser = argparse.ArgumentParser(description='Allow add taxonomy informationsa blast file')
-parser.add_argument("-o", "--out_file", help="The name of the output filtred file")
-parser.add_argument("-Nr", "Nr_results", help="The Nr table result file")
+parser = argparse.ArgumentParser(description='Allow add taxonomy informations to blast file')
+parser.add_argument("-bl", "--blast", help="blast file strand summary") 
+parser.add_argument("-Nr", "--Nr_blast", help="The Nr table result file after adding taxid info") 
+parser.add_argument("-fa", "--fasta", help="Fasta file of candidate loci") 
+parser.add_argument("-o", "--out_file", help="Nr blast file filtered") 
+parser.add_argument("-ofa", "--out_fasta", help="Fasta file of candidate loci filtered")
 args = parser.parse_args()
 
-#Usage example Filter_loci_with_NR.py -Nr /beegfs/data/bguinet/these/Results/Candidate_viral_loci_Nr.m8 -o /beegfs/data/bguinet/these/Results/Candidate_viral_loci_filtred.m8
-Blast_file= args.loci_file
-out_file=args.out_file
-
-
-#Gather all blast result together 
-Species_name_file= "/beegfs/data/bguinet/these/Species_genome_names.txt" #each line correspond to the species name 
-
-list_of_names=[]
-for names in open(Species_name_file,"r"):
-        list_of_names.append(names.replace("\n", ""))
-#
-
-ALLblast= pd.DataFrame(columns=['query','target','pident','alnlen','mismatch','gapopen','qstart','qend','qlen','tstart','tend','tlen','evalue','bits','strand','Newqstart',	'Newqend'])
-
-for sp in list_of_names:
-  blast=pd.read_csv("/beegfs/data/bguinet/these/Genomes/"+sp+"/run_mmseqs2/result_mmseqs2_strand_summary_V.m8",sep=";")
-  ALLblast=ALLblast.append(blast)
-  
-
-tab_Nr= pd.read_csv("/beegfs/data/bguinet/these/Clustering4/Candidate_loci_filtred_end_reciprocal_result_filtred_taxid.m8",sep=";")
+#Open mmseqs2 NR results file 
+NR_blast=pd.read_csv(arg.Nr_blast,sep=";")
 
 #If only viruses hits keep all clusters 
-tab_Nr = tab_Nr.loc[~(tab_Nr['evalue'].gt(0.0000001) & tab_Nr['superkingdom'].isin(['Eukaryota', 'Archaea', 'Bacteria']))]
+NR_blast = NR_blast.loc[~(NR_blast['evalue'].gt(0.0000001) & NR_blast['superkingdom'].isin(['Eukaryota', 'Archaea', 'Bacteria']))]
 
 #Sort evalues 
-tab_Nr=tab_Nr.sort_values(by='evalue', ascending=True)
-NaN_tab_Nr = tab_Nr.loc[tab_Nr['species'].isna()]
-tab_Nr = tab_Nr.drop_duplicates(subset=['query', 'species'], keep='first')
-tab_Nr = tab_Nr.append(NaN_tab_Nr)
-tab_Nr.superkingdom = tab_Nr.superkingdom.fillna('Unknown')
+NR_blast=NR_blast.sort_values(by='evalue', ascending=True)
+NaN_NR_blast = NR_blast.loc[NR_blast['species'].isna()]
+NR_blast = NR_blast.drop_duplicates(subset=['query', 'species'], keep='first')
+NR_blast = NR_blast.append(NaN_NR_blast)
+NR_blast.superkingdom = NR_blast.superkingdom.fillna('Unknown')
 
 #Create table with Nr match informations 
 #Take into account only non-hymenoptera matches 
-tab_Nr['count_Hymenoptera'] = tab_Nr['order'].eq('Hymenoptera').groupby(tab_Nr['query']).transform('sum')
-tab_Nr= tab_Nr.loc[~tab_Nr['order'].str.contains("Hymenoptera",na=False)]
-tab_Nr = tab_Nr.join(pd.crosstab(tab_Nr['query'], tab_Nr['superkingdom']).add_prefix('Count_'), on='query')
-tab_Nr['Count_Bacteria_Eukaryota_Archeae'] = tab_Nr['Count_Eukaryota'] + tab_Nr['Count_Bacteria']+ tab_Nr['Count_Archaea']
-tab_Nr['Dif_Bacteria_Eukaryota_Archeae_Viruses'] = tab_Nr['Count_Bacteria_Eukaryota_Archeae'] - tab_Nr['Count_Viruses']
+NR_blast['count_Diptera'] = NR_blast['order'].eq('Diptera').groupby(NR_blast['query']).transform('sum')
+NR_blast= NR_blast.loc[~NR_blast['order'].str.contains("Diptera",na=False)]
+NR_blast = NR_blast.join(pd.crosstab(NR_blast['query'], NR_blast['superkingdom']).add_prefix('Count_'), on='query')
+NR_blast['Count_Bacteria_Eukaryota_Archeae'] = NR_blast['Count_Eukaryota'] + NR_blast['Count_Bacteria']+ NR_blast['Count_Archaea']
+NR_blast['Dif_Bacteria_Eukaryota_Archeae_Viruses'] = NR_blast['Count_Bacteria_Eukaryota_Archeae'] - NR_blast['Count_Viruses']
 
 #Remove loci if to much bacteria hits compared to viruses (>20 +) and if less then 30 viruses hits 
-tab_Nr_remove1=tab_Nr.loc[tab_Nr['Count_Bacteria_Eukaryota_Archeae'].ge(10)]
-tab_Nr_remove1=tab_Nr_remove1.loc[~tab_Nr_remove1['Dif_Bacteria_Eukaryota_Archeae_Viruses'].lt(20)]
-tab_Nr_remove1=tab_Nr_remove1.loc[~tab_Nr_remove1['Count_Viruses'].gt(30)]
+NR_blast_remove1=NR_blast.loc[NR_blast['Count_Bacteria_Eukaryota_Archeae'].ge(10)]
+NR_blast_remove1=NR_blast_remove1.loc[~NR_blast_remove1['Dif_Bacteria_Eukaryota_Archeae_Viruses'].lt(20)]
+NR_blast_remove1=NR_blast_remove1.loc[~NR_blast_remove1['Count_Viruses'].gt(30)]
 
 #If less then 3 virus hit and more then 5 euk hit remove 
-tab_Nr_remove2=tab_Nr.loc[tab_Nr['Count_Viruses'].lt(3) & tab_Nr['Count_Bacteria_Eukaryota_Archeae'].ge(5)]
-tab_Nr_remove2=tab_Nr_remove2.loc[~tab_Nr_remove2['Count_Bacteria_Eukaryota_Archeae'].lt(15)]
+NR_blast_remove2=NR_blast.loc[NR_blast['Count_Viruses'].lt(3) & NR_blast['Count_Bacteria_Eukaryota_Archeae'].ge(5)]
+NR_blast_remove2=NR_blast_remove2.loc[~NR_blast_remove2['Count_Bacteria_Eukaryota_Archeae'].lt(15)]
 
 #If more than 100 bacteria hits, remove 
-tab_Nr_remove3=tab_Nr.loc[tab_Nr['Count_Bacteria'].gt(100)]
+NR_blast_remove3=NR_blast.loc[NR_blast['Count_Bacteria'].gt(100)]
 
-#Create a table 'tab_Nr_remove' composed of false positves loci 
-tab_Nr_remove=tab_Nr_remove1.append(tab_Nr_remove2)
-tab_Nr_remove=tab_Nr_remove.append(tab_Nr_remove3)
+#Create a table 'NR_blast_remove' composed of false positves loci 
+NR_blast_remove=NR_blast_remove1.append(NR_blast_remove2)
+NR_blast_remove=NR_blast_remove.append(NR_blast_remove3)
 
 #Keep unknown loci 
-tab_unknown = tab_Nr.loc[~tab_Nr['query'].isin(tab_Nr_remove['query'])]
+tab_unknown = NR_blast.loc[~NR_blast['query'].isin(NR_blast_remove['query'])]
 tab_unknown = tab_unknown.drop_duplicates(subset = 'query', keep = 'first')
 
 list_unknown= list(tab_unknown.loc[tab_unknown['Count_Viruses'].lt(3) & tab_unknown['Count_Unknown'].gt(1)]['query'].unique())
 
 #Keep one representing loci to remove 
-tab_Nr= tab_Nr.drop_duplicates(subset = 'query', keep = 'first')
+NR_blast= NR_blast.drop_duplicates(subset = 'query', keep = 'first')
 
-#Remove from the original blast file 
-blast =blast.loc[~blast['query'].isin(tab_Nr['query'])]
+out_file=args.out_file
+NR_blast.to_csv(out_file,sep=";",Index=FALSE)
 
-blast.to_csv(output_file,sep=";",indexe=false)
+#Filter fasta file of candidate loci using filtered Blast
+Fasta_file=open(args.fasta, "r")
+record_dict = SeqIO.to_dict(SeqIO.parse(Fasta_file, "fasta"))
+
+with open(args.out_fasta,"w") as output:
+    for i in Blast_filtered['query'].unique():
+        print('>',record_dict['query'].id,sep="",file=output)
+        print(record_dict['query'].seq,file=output)
